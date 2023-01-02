7f99ed2249bf1b6821e3f9827fe3d31551c0b1cf
rgyan-intern <intern@rgyan.com>
2023-01-02 18:01:54 +0530

    Dark Theme toggler made using Provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:stat_management/provider/count_provider.dart';
+import 'package:stat_management/provider/dark_theme_provider.dart';
 import 'package:stat_management/provider/example_one_provider.dart';
+import 'package:stat_management/provider/favourite_provider.dart';
 import 'package:stat_management/screen/count_screen.dart';
+import 'package:stat_management/screen/dark_theme.dart';
 import 'package:stat_management/screen/example_one.dart';
 import 'package:stat_management/screen/favourite_screen.dart';
 import 'package:stat_management/state.dart';
@@ -18,15 +21,22 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-    providers: [
-      ChangeNotifierProvider(create: (_) => CountProvider()),
-      ChangeNotifierProvider(create: (_) => ExampleOneProvider())
- ,   ],
-child:  MaterialApp(
-  home: FavouriteScreen()
-),
-    );
+        providers: [
+          ChangeNotifierProvider(create: (_) => CountProvider()),
+          ChangeNotifierProvider(create: (_) => ExampleOneProvider()),
+          ChangeNotifierProvider(create: (_) => FavouriteItemProvider()),
+          ChangeNotifierProvider(create: (_) => ThemeChanger()),
+        ],
+        child: Builder(builder: (BuildContext context) {
+          final themeChanger = Provider.of<ThemeChanger>(context);
+          return MaterialApp(
+              themeMode: themeChanger.themeMode,
+              theme: ThemeData(brightness: Brightness.light, primarySwatch: Colors.blue),
+              darkTheme: ThemeData(brightness: Brightness.dark,primarySwatch: Colors.lightBlue,
+              appBarTheme: AppBarTheme(
+                backgroundColor: Colors.teal
+              )),
+              home: DarkThemeScreen());
+        }));
   }
 }
-
-
--- lib/screen/myFavourite_screen.dart
+++ lib/screen/myFavourite_screen.dart
@@ -1,36 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:stat_management/screen/myFavourite_screen.dart';
 
 import '../provider/favourite_provider.dart';
 
-class FavouriteScreen extends StatefulWidget {
-  const FavouriteScreen({Key? key}) : super(key: key);
+class MyFavouriteScreen extends StatefulWidget {
+  const MyFavouriteScreen({Key? key}) : super(key: key);
 
   @override
-  State<FavouriteScreen> createState() => _FavouriteScreenState();
+  State<MyFavouriteScreen> createState() => _MyFavouriteScreenState();
 }
 
-class _FavouriteScreenState extends State<FavouriteScreen> {
-  List<int> selecteditem = [];
+class _MyFavouriteScreenState extends State<MyFavouriteScreen> {
+
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
+   final favouriteProvider = Provider.of<FavouriteItemProvider>(context);
+    return Scaffold(
         appBar: AppBar(
           title: Text("Favourite App"),
-          actions: [
-            InkWell(
-                onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) => MyFavouriteScreen()));
-   },child: Icon(Icons.favorite)),
-                
-            SizedBox(width: 20,)
-          ],
+          // actions: [
+          //   InkWell(
+          //       onTap: () {
+          //         Navigator.push(context, MaterialPageRoute(builder: (context) => MyFavouriteScreen()));
+          //       },child: Icon(Icons.favorite)),
+          //
+          //   SizedBox(width: 20,)
+          // ],
         ),
         body: Column(
           children: [
             Expanded(
-              child: ListView.builder(itemCount:10,itemBuilder: (context, index) {
+              child: ListView.builder(itemCount:favouriteProvider.selectedItem.length,itemBuilder: (context,
+                  index) {
                 return Consumer<FavouriteItemProvider>(builder: (context,value,child){
                   return  ListTile(
                     onTap: (){
@@ -52,4 +53,6 @@ class _FavouriteScreenState extends State<FavouriteScreen> {
           ],
         ));
   }
+
 }
+
