5513f91e1d0c63f6efda885e5100088ce70e9708
rgyan-intern <intern@rgyan.com>
2023-01-02 12:29:10 +0530

    consumer added

--- lib/screen/count_screen.dart
+++ lib/screen/count_screen.dart
@@ -12,16 +12,25 @@ class Count extends StatefulWidget {
 class _CountState extends State<Count> {
   @override
   Widget build(BuildContext context) {
-    final countProvider = Provider.of<CountProvider>(context);
-    return Scaffold(
-      appBar: AppBar(title: const Text("State"),),
-      body: Center(child: Text(countProvider.count.toString(),style: TextStyle(fontSize: 50),),),
-      floatingActionButton: FloatingActionButton(
-        onPressed:(){
-          countProvider.setCount();
-        } ,
-        child: const Icon(Icons.add),
+    final countProvider = Provider.of<CountProvider>(context,listen: false);
 
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("State"),
+      ),
+      body: Center(
+        child: Consumer<CountProvider>(builder: (context,value,child){
+          return Text(
+            value.count.toString(),
+            style: TextStyle(fontSize: 50),
+          );
+        },)
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          countProvider.setCount();
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
